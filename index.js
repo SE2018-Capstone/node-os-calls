aaa2a79af31649b8be2895caa3212c6182d2fe85
Sam Maier <sam@sammaier.ca>
2016-12-18 17:22:39 -0500

    Bindings module doesn't work with webpack - try #2

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-module.exports = require('bindings')('node_os_calls');
+const path = require('path');
+// Need path to look like ./build/Release/node_os_calls and work on all systems
+module.exports = require("." + path.sep + path.join('build', 'Release', 'node_os_calls'));
