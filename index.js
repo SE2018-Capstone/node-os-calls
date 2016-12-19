05b1ec9467f24d69e4f627b307012137a6f5dcd7
Sam Maier <sam@sammaier.ca>
2016-12-18 21:37:54 -0500

    Updated index working on windows

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-const path = require('path');
-// Need path to look like ./build/Release/node_os_calls and work on all systems
-module.exports = require("." + path.sep + path.join('build', 'Release', 'node_os_calls'));
+module.exports = require('./build/Release/node_os_calls');
