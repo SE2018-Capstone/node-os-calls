fd092f2339419f3d8bf945c131b0190b67b41374
Sam Maier <sam@sammaier.ca>
2016-12-19 00:37:15 -0500

    Windows system call WORKING

--- example.cc
+++ example.cc
@@ -1,36 +1,50 @@
 #include <nan.h>
 
-const int maxValue = 10;  
+#include <stdio.h>
+#include <windows.h>
+#include <mmdeviceapi.h>
+#include <endpointvolume.h>
+
+
+const int maxValue = 10;
 int numberOfCalls = 0;
 
-NAN_METHOD(WhoAmI) {  
+NAN_METHOD(whoAmI) {
   auto message = Nan::New<v8::String>("I'm a Node Hero!").ToLocalChecked();
   info.GetReturnValue().Set(message);
 }
 
-NAN_METHOD(Increment) {  
-  if (!info[0]->IsNumber()) {
-    Nan::ThrowError("Argument must be a number");
-    return;
-  }
+NAN_METHOD(getVolume)
+{
+  HRESULT hr;
+  // -------------------------
+  CoInitialize(NULL);
+  IMMDeviceEnumerator *deviceEnumerator = NULL;
+  hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_INPROC_SERVER, __uuidof(IMMDeviceEnumerator), (LPVOID *)&deviceEnumerator);
+  IMMDevice *defaultDevice = NULL;
 
-  double infoValue = info[0]->NumberValue();
-  if (numberOfCalls + infoValue > maxValue) {
-    Nan::ThrowError("Counter went through the roof!");
-    return;
-  }
+  hr = deviceEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &defaultDevice);
+  deviceEnumerator->Release();
+  deviceEnumerator = NULL;
 
-  numberOfCalls += infoValue;
+  IAudioEndpointVolume *endpointVolume = NULL;
+  hr = defaultDevice->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_INPROC_SERVER, NULL, (LPVOID *)&endpointVolume);
+  defaultDevice->Release();
+  defaultDevice = NULL;
 
-  auto currentNumberOfCalls =
-    Nan::New<v8::Number>(numberOfCalls);
+  // -------------------------
+  float currentVolume = 0;
+  hr = endpointVolume->GetMasterVolumeLevelScalar(&currentVolume);
+  printf("Current volume as a scalar is: %f\n", currentVolume);
+  endpointVolume->Release();
+  CoUninitialize();
 
-  info.GetReturnValue().Set(currentNumberOfCalls);
+  info.GetReturnValue().Set(Nan::New<v8::Number>(currentVolume));
 }
 
-NAN_MODULE_INIT(Initialize) {  
-  NAN_EXPORT(target, WhoAmI);
-  NAN_EXPORT(target, Increment);
+NAN_MODULE_INIT(Initialize) {
+  NAN_EXPORT(target, whoAmI);
+  NAN_EXPORT(target, getVolume);
 }
 
 NODE_MODULE(node_os_calls, Initialize)
